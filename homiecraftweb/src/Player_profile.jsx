a0fa0d64e9eddad0f358d6de85d349a305e42bfc
Skullable <riyaanbinusman57@gmail.com>
2023-05-24 16:48:20 +0500

    removed function httpget

--- homiecraftweb/src/Player_profile.jsx
+++ homiecraftweb/src/Player_profile.jsx
@@ -2,36 +2,12 @@ import './Player_profile.scss'
 import Players from './Players';
 
 
-function httpGet(theUrl) {
-    const proxyUrl = 'https://api.allorigins.win/raw?url='; // CORS proxy URL
-    const urlWithProxy = `${proxyUrl}${theUrl}`;
-  
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('GET', urlWithProxy, false); // false for synchronous request
-    xmlHttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); // Set the 'X-Requested-With' header
-    try {
-      xmlHttp.send(null);
-      if (xmlHttp.status >= 200 && xmlHttp.status < 300) {
-        return xmlHttp.responseText;
-      } else {
-        throw new Error(`HTTP status ${xmlHttp.status}`);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      // Handle the error here or throw it to be handled at a higher level
-      throw error;
-    }
-  }
-  
-
-
 export default function Player_profile({ menuOpen,setMenuOpen ,...props }) {
-    const link = 'https://api.mojang.com/users/profiles/minecraft/'
-    const skinlink = 'https://visage.surgeplay.com/full/'
+//    const link = 'https://api.minetools.eu/uuid/'
+//   const skinlink = 'https://visage.surgeplay.com/full/'
+    const skinlink = 'https://mineskin.eu/armor/body/'
+    const skinlink2 = '/80.png'
     const skindownload = 'https://minotar.net/download/'
-    var response = httpGet(`${link}${props.name}`)
-    var result = JSON.parse(response)
-    var UUID = result.id
     
 
   return (
@@ -41,12 +17,11 @@ export default function Player_profile({ menuOpen,setMenuOpen ,...props }) {
         <h1 className='heading'>Player info</h1>
         <div className="wrapper">
         <div className="skinwrapper">
-            <img src={`${skinlink}${UUID}`} alt='' className="skin"></img>
+            <img src={`${skinlink}${props.name}${skinlink2}`} alt='Skin généré par Minecraft-api.com' className="skin"></img>
             <div className="downloadbtn"><a className='dllink' href={`${skindownload}${props.name}`}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" margin='5px' fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"> <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/> <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/> </svg>     DOWNLOAD SKIN</a></div>
         </div>
         <div className="playerinfo">
             <div className="txt"><h2 className='h2'>Name:  {props.name}</h2></div>
-            <div className="txt"><h2 className='h2'>UUID:  {UUID}</h2></div>
             <div className="txt"><h2 className='h2'>Description:</h2></div>
         </div>
         </div>
